b389fcfa21358a4fdeb60ef44689c6be0242af71
Ohemaa Appiatu <ohemappiatu@gmail.com>
2025-11-05 22:17:24 +0000

    add basic_operationsscript

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#defining variables
+number1 = 10
+number2 = 5
+
+#operations
+addition = number1 + number2
+subtraction = number2 - number1
+multiplication = number1 * number2
+
+#results
+print(f'Addition of {number1} and {number2} is {addition}')
+print(f'Subtraction of {number1} and {number2} is {subtraction}')
+print(f'Multiplication of {number1} and {number2} is {multiplication}')
